a1abbab33e63213515501107d9a64c1cf0b5cbe0
KilluaChen <larry_e@163.com>
2017-04-26 17:06:10 +0800

    rename

--- resource/jsonFormater.js
+++ resource/jsonFormater.js
@@ -4,8 +4,8 @@ function JsonFormater(opt) {
         tabSize: 2,
         singleTab: "  ",
         quoteKeys: true,
-        imgCollapsed: "https://larry-666.github.io/jsonformat/images/Collapsed.gif",
-        imgExpanded: "https://larry-666.github.io/jsonformat/images/Expanded.gif",
+        imgCollapsed: "https://killuachen.github.io/jsonformat/images/Collapsed.gif",
+        imgExpanded: "https://killuachen.github.io/jsonformat/images/Expanded.gif",
         isCollapsible: true
     }, opt || {});
     this.isFormated = false;
